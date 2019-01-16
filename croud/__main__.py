a7df2fa10fd564f75cf8255037de10030aa49830
Lukas Bals <mail@lukasbals.at>
2019-01-16 11:34:39 +0100

    implemented croud users roles list command (#47)

--- croud/__main__.py
+++ croud/__main__.py
@@ -43,6 +43,7 @@ from croud.organizations.create import organizations_create
 from croud.organizations.list import organizations_list
 from croud.projects.list import projects_list
 from croud.users.roles.add import roles_add
+from croud.users.roles.list import roles_list
 
 
 def main():
@@ -129,7 +130,12 @@ def main():
                                 role_fqn_arg,
                             ],
                             "calls": roles_add,
-                        }
+                        },
+                        "list": {
+                            "description": "Lists all roles a user can be assigned to",
+                            "extra_args": [output_fmt_arg],
+                            "calls": roles_list,
+                        },
                     },
                 }
             },
