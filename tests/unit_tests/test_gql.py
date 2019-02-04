4cd8e56f4593427351c83ac1f5a874cd9c124cd6
Alastair Appleby <alastair@crate.io>
2019-02-04 14:46:38 +0100

    Added pytest.parametrize

--- tests/unit_tests/test_gql.py
+++ tests/unit_tests/test_gql.py
@@ -21,90 +21,83 @@
 
 import unittest
 from argparse import Namespace
-from unittest import mock
+from unittest.mock import patch
+
+import pytest
 
 from croud.config import Configuration
 from croud.exceptions import GQLError
 from croud.gql import Query
 
 
-class TestHttpSession(unittest.TestCase):
+@pytest.mark.parametrize(
+    "input,expected", [(Namespace(env="dev"), "dev"), (Namespace(env=None), "prod")]
+)
+@patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+def test_env_set(mock_load_config, input, expected):
+    query = Query("{}", input)
+    assert query._env == expected
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (Namespace(env="dev", output_fmt="json"), "json"),
+        (Namespace(env="dev", output_fmt=None), "table"),
+        (Namespace(env="dev"), "table"),
+    ],
+)
+@patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+def test_output_fmt_set(mock_load_config, input, expected):
+    query = Query("{}", input)
+    assert query._output_fmt == expected
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (Namespace(env="dev", region="westeurope.azure"), "westeurope.azure"),
+        (Namespace(env="dev", region=None), "bregenz.a1"),
+        (Namespace(env="dev"), "bregenz.a1"),
+    ],
+)
+@patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+def test_region_set(mock_load_config, input, expected):
+    query = Query("{}", input)
+    assert query._region == expected
+
+
+class TestGQL(unittest.TestCase):
     _data = {"data": {"key": "value"}}
     _error_data = {"errors": [{"message": "error"}]}
 
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    def test_env_set(self, mock_load_config):
-        args = Namespace(env="dev")
-        query = Query("{}", args)
-
-        self.assertEqual("dev", query._env)
-
-        args = Namespace(env=None)
-        query = Query("{}", args)
-
-        self.assertEqual("prod", query._env)
-
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    def test_output_fmt_set(self, mock_load_config):
-        args = Namespace(env="dev", output_fmt="json")
-        query = Query("{}", args)
-
-        self.assertEqual("json", query._output_fmt)
-
-        args = Namespace(env="dev", output_fmt=None)
-        query = Query("{}", args)
-
-        self.assertEqual("table", query._output_fmt)
-
-        args = Namespace(env="dev")
-        query = Query("{}", args)
-
-        self.assertEqual("table", query._output_fmt)
-
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    def test_region_set(self, mock_load_config):
-        args = Namespace(env="dev", region="westeurope.azure")
-        query = Query("{}", args)
-
-        self.assertEqual("westeurope.azure", query._region)
-
-        args = Namespace(env="dev", region=None)
-        query = Query("{}", args)
-
-        self.assertEqual("bregenz.a1", query._region)
-
-        args = Namespace(env="dev")
-        query = Query("{}", args)
-
-        self.assertEqual("bregenz.a1", query._region)
-
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    @mock.patch.object(Query, "run", return_value=_data)
+    @patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+    @patch.object(Query, "run", return_value=_data)
     def test_get_rows(self, mock_run, mock_load_config):
         query = Query("{}", Namespace(env="dev"))
         data = query._get_rows()
 
         self.assertEqual(self._data["data"], data)
 
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    @mock.patch.object(Query, "run", return_value=_error_data)
+    @patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+    @patch.object(Query, "run", return_value=_error_data)
     def test_get_rows_error(self, mock_run, mock_load_config):
         query = Query("{}", Namespace(env="dev"))
 
         with self.assertRaises(GQLError):
             query._get_rows()
 
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    @mock.patch.object(Query, "_get_rows", return_value=_data["data"])
+    @patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+    @patch.object(Query, "_get_rows", return_value=_data["data"])
     def test_check_rows(self, mock_get_rows, mock_load_config):
         query = Query("{}", Namespace(env="dev"))
         data = query._get_rows()
 
         self.assertEqual(True, query._check_rows(data))
 
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    @mock.patch("croud.gql.print_info")
-    @mock.patch.object(Query, "_get_rows", return_value=[])
+    @patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+    @patch("croud.gql.print_info")
+    @patch.object(Query, "_get_rows", return_value=[])
     def test_check_rows_empty(self, mock_get_rows, mock_print_info, mock_load_config):
         query = Query("{}", Namespace(env="dev"))
         data = query._get_rows()
@@ -114,9 +107,9 @@ class TestHttpSession(unittest.TestCase):
 
         mock_print_info.assert_called_once_with("Result contained no data to print.")
 
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    @mock.patch("croud.gql.print_error")
-    @mock.patch.object(Query, "_get_rows", return_value="string")
+    @patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+    @patch("croud.gql.print_error")
+    @patch.object(Query, "_get_rows", return_value="string")
     def test_check_rows_bad_format(
         self, mock_get_rows, mock_print_error, mock_load_config
     ):
@@ -129,18 +122,18 @@ class TestHttpSession(unittest.TestCase):
         message = "Result has no proper format to print."
         mock_print_error.assert_called_once_with(message)
 
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    @mock.patch("croud.gql.print_format")
-    @mock.patch.object(Query, "_get_rows", return_value=_data["data"])
+    @patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+    @patch("croud.gql.print_format")
+    @patch.object(Query, "_get_rows", return_value=_data["data"])
     def test_print_result(self, mock_get_rows, mock_print_format, mock_load_config):
         query = Query("{}", Namespace(env="dev"))
         query.print_result("key")
 
         mock_print_format.assert_called_once_with(self._data["data"]["key"], "table")
 
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    @mock.patch("croud.gql.print_error")
-    @mock.patch.object(Query, "run", return_value=_error_data)
+    @patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+    @patch("croud.gql.print_error")
+    @patch.object(Query, "run", return_value=_error_data)
     def test_print_result_error(self, mock_run, mock_print_error, mock_load_config):
         query = Query("{}", Namespace(env="dev"))
         query.print_result("key")
@@ -148,9 +141,9 @@ class TestHttpSession(unittest.TestCase):
         message = self._error_data["errors"][0]["message"]
         mock_print_error.assert_called_once_with(message)
 
-    @mock.patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
-    @mock.patch("croud.gql.print_info")
-    @mock.patch.object(Query, "run", return_value={"data": []})
+    @patch("croud.config.load_config", return_value=Configuration.DEFAULT_CONFIG)
+    @patch("croud.gql.print_info")
+    @patch.object(Query, "run", return_value={"data": []})
     def test_print_result_empty(self, mock_get_rows, mock_print_info, mock_load_config):
         query = Query("{}", Namespace(env="dev"))
         query.print_result("key")
