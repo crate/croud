749934938a67d541ec65e19dcc29155938f32b12
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-05-06 16:53:05 +0200

    Prune config (no longer needed)

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,6 @@
 from crate.theme.rtd.conf.cloud_cli import *  # noqa
 
-exclude_patterns = [".*", "*.lint"]
-
-# crate.theme sets html_favicon to favicon.png which causes a warning because
-# it should be a .ico and in addition there is no favicon.png in this project
-# so it can't find the file
-html_favicon = None
-
-site_url = "https://crate.io/docs/croud/en/latest/"
 extensions = ["sphinx_sitemap", "sphinxarg.ext"]
-
-# The file extension
-source_suffix = ".rst"
-
-# The master toctree document.
-master_doc = "index"
 
 html_static_path = ["_static"]
 html_context = {"extra_css_files": ["_static/sphinxarg.css"]}
