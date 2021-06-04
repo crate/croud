a3643f518a265c0ece25a35d45d1d03c3022c4e6
Hasan Ramezani <hasan.r67@gmail.com>
2021-06-04 09:16:36 +0200

    Add mypy type ignore to docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 from crate.theme.rtd.conf.cloud_cli import *  # noqa
 
-extensions += ["sphinx_sitemap", "sphinxarg.ext"]  # noqa:F405
+extensions += ["sphinx_sitemap", "sphinxarg.ext"]  # type:ignore # noqa:F405
 
 html_static_path = ["_static"]
 html_context = {"extra_css_files": ["_static/sphinxarg.css"]}
