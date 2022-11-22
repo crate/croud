4ac76338158234ff07dc1d1abc09e87483cfbdbd
Romanas Sonkinas <romanas@crate.io>
2022-11-22 08:22:09 +0000

    Removed tox
    This does not seem to be used - one less requirement to deal with.

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setup(
         "shtab==1.5.8",
     ],
     extras_require={
-        "testing": ["tox==3.14.2"],
         "development": [
             "black==22.10.0",
             "flake8==3.7.9",
