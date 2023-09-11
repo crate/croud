4e5f7b7201dc73b1fc7edee7b018c31079218f40
Thomas Achatz <thomas.achatz@crate.io>
2023-09-11 14:50:02 +0200

    Prepare release 1.7.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 from pkg_resources.extern.packaging.version import Version
 from setuptools import find_packages, setup
 
-__version__ = Version("1.6.0")
+__version__ = Version("1.7.0")
 
 try:
     with open("README.rst", "r", encoding="utf-8") as f:
